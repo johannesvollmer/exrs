02852bb3bce7219de88f222f7b998bd92cad8120
johannesvollmer <johannes.boenki@t-online.de>
2020-02-16 19:25:23 +0100

    tidy up extract png example

--- examples/extract_pngs.rs
+++ examples/extract_pngs.rs
@@ -15,52 +15,98 @@ use std::io::ErrorKind;
 // FIXME throws "acces denied" sometimes, simply trying again usually works.
 //
 pub fn main() {
+    let path = "D:/Pictures/openexr/BeachBall/multipart.0001.exr";
+
     let now = ::std::time::Instant::now();
 
-    let path =
-        "D:/Pictures/openexr/BeachBall/multipart.0001.exr"
-
-
-//        "D:/Pictures/openexr/Tiles/Ocean.exr"
-//        "D:/Pictures/openexr/MultiResolution/Kapaa.exr"
-//        "D:/Pictures/openexr/MultiView/Impact.exr"
-//        "D:/Pictures/openexr/MultiResolution/KernerEnvCube.exr"
-//        "D:/Pictures/openexr/MultiResolution/Bonita.exr"
-
-
-//            "D:/Pictures/openexr/MultiResolution/Bonita.exr"
-
-//            "D:/Pictures/openexr/crowskull/crow_uncompressed.exr"
-//        "D:/Pictures/openexr/crowskull/crow_zips.exr"
-//            "D:/Pictures/openexr/crowskull/crow_rle.exr"
-//            "D:/Pictures/openexr/crowskull/crow_zip_half.exr"
-
-
-//        "D:/Pictures/openexr/v2/Stereo/Trunks.exr" // deep data, stereo
-    ;
-
+    // load the exr file from disk with multicore decompression
     let image = Image::read_from_file(path, read_options::high()).unwrap();
 
     // warning: highly unscientific benchmarks ahead!
     let elapsed = now.elapsed();
     let millis = elapsed.as_secs() * 1000 + elapsed.subsec_millis() as u64;
-    println!("\ndecoded exr file in {:?}s", millis as f32 * 0.001);
+    println!("\nDecoded exr file in {:?}s", millis as f32 * 0.001);
 
+    {   // clear output directory
+        if let Err(error) = fs::remove_dir_all("testout") {
+            if error.kind() != ErrorKind::NotFound {
+                println!("{}", error);
+            }
+        }
 
-    /// convert raw float data to a png, doing automatic brightness adjustments
+        fs::create_dir("testout").unwrap();
+    }
+
+    println!("Writing PNG images...");
+
+    for (layer_index, layer) in image.layers.iter().enumerate() {
+        let layer_name = layer.attributes.name.as_ref()
+            .map_or(String::from("1"), attributes::Text::to_string);
+
+        for channel in &layer.channels {
+            match &channel.content {
+                ChannelData::F16(levels) => {
+                    let levels = levels.as_flat_samples()
+                        .expect("deep data to png not supported");
+
+                    for sample_block in levels.as_slice() {
+                        let data : Vec<f32> = sample_block.samples.iter().map(|f16| f16.to_f32()).collect();
+
+                        save_f32_image_as_png(&data, sample_block.resolution, format!(
+                            "testout/{} ({}) {}_f16_{}x{}.png",
+                            layer_index, layer_name, channel.name,
+                            sample_block.resolution.0, sample_block.resolution.1,
+                        ))
+                    }
+                },
+
+                ChannelData::F32(levels) => {
+                    let levels = levels.as_flat_samples()
+                        .expect("deep data to png not supported");
+
+                    for sample_block in levels.as_slice() {
+                        save_f32_image_as_png(&sample_block.samples, sample_block.resolution, format!(
+                            "testout/{} ({}) {}_f32_{}x{}.png",
+                            layer_index, layer_name, channel.name,
+                            sample_block.resolution.0, sample_block.resolution.1,
+                        ))
+                    }
+                },
+
+                ChannelData::U32(levels) => {
+                    let levels = levels.as_flat_samples()
+                        .expect("deep data to png not supported");
+
+                    for sample_block in levels.as_slice() {
+                        let data : Vec<f32> = sample_block.samples.iter().map(|value| *value as f32).collect();
+
+                        save_f32_image_as_png(&data, sample_block.resolution, format!(
+                            "testout/{} ({}) {}_u32_{}x{}.png",
+                            layer_index, layer_name, channel.name,
+                            sample_block.resolution.0, sample_block.resolution.1,
+                        ))
+                    }
+                },
+            }
+        }
+    }
+
+    /// Save raw float data to a PNG file, doing automatic brightness adjustments per channel
     fn save_f32_image_as_png(data: &[f32], size: Vec2<usize>, name: String) {
         let mut png_buffer = image::GrayImage::new(size.0 as u32, size.1 as u32);
         let mut sorted = Vec::from(data);
         sorted.sort_by(|a, b| a.partial_cmp(b).unwrap_or(Ordering::Less));
 
-        // sixth percentile normalization
+        // percentile normalization
         let max = sorted[7 * sorted.len() / 8];
         let min = sorted[1 * sorted.len() / 8];
 
+        // primitive tone mapping
         let tone = |v: f32| (v - 0.5).tanh() * 0.5 + 0.5;
         let max_toned = tone(*sorted.last().unwrap());
         let min_toned = tone(*sorted.first().unwrap());
 
+        // for each pixel, tone map the value
         for (x, y, pixel) in png_buffer.enumerate_pixels_mut() {
             let v = data[(y as usize * size.0 + x as usize)];
             let v = (v - min) / (max - min);
@@ -73,66 +119,6 @@ pub fn main() {
         png_buffer.save(&name).unwrap();
     }
 
-    if let Err(error) = fs::remove_dir_all("testout") {
-        if error.kind() != ErrorKind::NotFound {
-            println!("{}", error);
-        }
-    }
-
-    fs::create_dir("testout").unwrap();
-
-    for (layer_index, layer) in image.layers.iter().enumerate() {
-        for channel in &layer.channels {
-            match &channel.content {
-                ChannelData::F16(levels) => {
-                    let levels = levels.as_flat_samples()
-                        .expect("deep data to png not supported");
-
-                    for sample_block in levels.as_slice() {
-                        let data : Vec<f32> = sample_block.samples.iter().map(|f16| f16.to_f32()).collect();
-
-                        save_f32_image_as_png(&data, sample_block.resolution, format!(
-                            "testout/{} ({}) {}_f16_{}x{}.png",
-                            layer_index,
-                            layer.attributes.name.as_ref().map(attributes::Text::to_string).unwrap_or(String::from("1")),
-                            channel.name,
-                            sample_block.resolution.0,
-                            sample_block.resolution.1,
-                        ))
-                    }
-                },
-
-                ChannelData::F32(levels) => {
-                    let levels = levels.as_flat_samples().unwrap();
-                    for sample_block in levels.as_slice() {
-                        save_f32_image_as_png(&sample_block.samples, sample_block.resolution, format!(
-                            "testout/{} ({}) {}_f16_{}x{}.png",
-                            layer_index,
-                            layer.attributes.name.as_ref().map(attributes::Text::to_string).unwrap_or(String::from("1")),
-                            channel.name,
-                            sample_block.resolution.0,
-                            sample_block.resolution.1,
-                        ))
-                    }
-                },
-
-                ChannelData::U32(levels) => {
-                    let levels = levels.as_flat_samples().unwrap();
-                    for sample_block in levels.as_slice() {
-                        let data : Vec<f32> = sample_block.samples.iter().map(|value| *value as f32).collect();
-
-                        save_f32_image_as_png(&data, sample_block.resolution, format!(
-                            "testout/{} ({}) {}_f16_{}x{}.png",
-                            layer_index,
-                            layer.attributes.name.as_ref().map(attributes::Text::to_string).unwrap_or(String::from("1")),
-                            channel.name,
-                            sample_block.resolution.0,
-                            sample_block.resolution.1,
-                        ))
-                    }
-                },
-            }
-        }
-    }
+    println!("Saved PNG images.");
 }
 
