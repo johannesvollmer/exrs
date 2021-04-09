2c4948701673f12535163880380e477732dff2b9
Johannes Vollmer <32042925+johannesvollmer@users.noreply.github.com>
2021-04-09 18:39:35 +0200

    fix benchmarks (adjust to new api)

--- benches/profiling.rs
+++ benches/profiling.rs
@@ -2,42 +2,48 @@
 extern crate bencher;
 
 extern crate exr;
-use exr::prelude::common::*;
-use exr::prelude::rgba_image as rgb;
+use exr::prelude::*;
 
 use bencher::Bencher;
 use std::fs;
+use std::io::Cursor;
 
 /// Read image from file
-fn read_single_image(bench: &mut Bencher) {
+fn read_single_image_all_channels(bench: &mut Bencher) {
     let path = "tests/images/valid/custom/crowskull/crow_uncompressed.exr";
 
     bench.iter(||{
-        rgb::ImageInfo::read_pixels_from_file(
-            path, read_options::low(),
-            rgb::pixels::create_flattened_f16,
-            rgb::pixels::flattened_pixel_setter()
-        ).unwrap();
+        let image = exr::prelude::read()
+            .no_deep_data().largest_resolution_level()
+            .all_channels()
+            .all_layers().all_attributes()
+            .non_parallel()
+            .from_file(path).unwrap();
+
+        bencher::black_box(image);
     })
 }
 
 /// Read image from in-memory buffer
-fn read_single_image_from_buffer(bench: &mut Bencher) {
+fn read_single_image_from_buffer_rgba_channels(bench: &mut Bencher) {
     let file = fs::read("tests/images/valid/custom/crowskull/crow_uncompressed.exr").unwrap();
 
     bench.iter(||{
-        rgb::ImageInfo::read_pixels_from_buffered(
-            std::io::Cursor::new(&file), read_options::low(),
-            rgb::pixels::create_flattened_f16,
-            rgb::pixels::flattened_pixel_setter()
-        ).unwrap();
+        let image = exr::prelude::read()
+            .no_deep_data().largest_resolution_level()
+            .rgba_channels(exr::image::pixel_vec::create_pixel_vec::<(f16,f16,f16,f16), _>, exr::image::pixel_vec::set_pixel_in_vec)
+            .all_layers().all_attributes()
+            .non_parallel()
+            .from_buffered(Cursor::new(file.as_slice())).unwrap();
+
+        bencher::black_box(image);
     })
 }
 
 
 benchmark_group!(profiling,
-    read_single_image_from_buffer,
-    read_single_image,
+    read_single_image_from_buffer_rgba_channels,
+    read_single_image_all_channels,
 );
 
 benchmark_main!(profiling);
--- benches/write.rs
+++ benches/write.rs
@@ -2,83 +2,65 @@
 extern crate bencher;
 
 extern crate exr;
-use exr::image::full::*;
+use exr::prelude::*;
 
 use bencher::Bencher;
 use std::io::Cursor;
-use exr::image::{read_options, write_options};
-use exr::prelude::common::*;
 
-/// Write with multi-core compression
-fn write_single_image_parallel(bench: &mut Bencher) {
+fn write_parallel_any_channels_to_buffered(bench: &mut Bencher) {
     let path = "tests/images/valid/custom/crowskull/crow_rle.exr";
-    let image = Image::read_from_file(path, read_options::high()).unwrap();
+    let image = read_all_flat_layers_from_file(path).unwrap();
 
     bench.iter(||{
         let mut result = Vec::new();
-        Image::write_to_buffered(&image, Cursor::new(&mut result), write_options::high()).unwrap();
+        image.write().to_buffered(Cursor::new(&mut result)).unwrap();
         bencher::black_box(result);
     })
 }
 
-/// Write with multi-core compression
-fn write_zip1(bench: &mut Bencher) {
+fn write_parallel_zip1_to_buffered(bench: &mut Bencher) {
     let path = "tests/images/valid/custom/crowskull/crow_rle.exr";
-    let mut image = Image::read_from_file(path, read_options::high()).unwrap();
-    for layer in &mut image.layers { layer.compression = Compression::ZIP1 };
+
+    let mut image = read_first_flat_layer_from_file(path).unwrap();
+    image.layer_data.encoding.compression = Compression::ZIP1;
 
     bench.iter(||{
         let mut result = Vec::new();
-        Image::write_to_buffered(&image, Cursor::new(&mut result), write_options::high()).unwrap();
-        println!("zip1 size: {}", result.len() as f32 / 1000000.0);
+        image.write().to_buffered(Cursor::new(&mut result)).unwrap();
         bencher::black_box(result);
     })
 }
 
-/// Write with multi-core compression
-fn write_zip16(bench: &mut Bencher) {
+fn write_parallel_zip16_to_buffered(bench: &mut Bencher) {
     let path = "tests/images/valid/custom/crowskull/crow_rle.exr";
-    let mut image = Image::read_from_file(path, read_options::high()).unwrap();
-    for layer in &mut image.layers { layer.compression = Compression::ZIP16 };
+
+    let mut image = read_first_flat_layer_from_file(path).unwrap();
+    image.layer_data.encoding.compression = Compression::ZIP16;
 
     bench.iter(||{
         let mut result = Vec::new();
-        Image::write_to_buffered(&image, Cursor::new(&mut result), write_options::high()).unwrap();
-        println!("zip16 size: {}", result.len() as f32 / 1000000.0);
+        image.write().to_buffered(Cursor::new(&mut result)).unwrap();
         bencher::black_box(result);
     })
 }
 
-/// Write with singlecore compression
-fn write_single_image(bench: &mut Bencher) {
-    let path = "tests/images/valid/custom/crowskull/crow_rle.exr";
-    let image = Image::read_from_file(path, read_options::high()).unwrap();
-
-    bench.iter(||{
-        let mut result = Vec::new();
-        Image::write_to_buffered(&image, Cursor::new(&mut result), write_options::low()).unwrap();
-        bencher::black_box(result);
-    })
-}
-
-/// Write with singlecore zip compression
-fn write_single_image_uncompressed(bench: &mut Bencher) {
+fn write_uncompressed_to_buffered(bench: &mut Bencher) {
     let path = "tests/images/valid/custom/crowskull/crow_uncompressed.exr";
-    let image = Image::read_from_file(path, read_options::high()).unwrap();
+    let image = read_all_flat_layers_from_file(path).unwrap();
+    assert!(image.layer_data.iter().all(|layer| layer.encoding.compression == Compression::Uncompressed));
 
     bench.iter(||{
         let mut result = Vec::new();
-        Image::write_to_buffered(&image, Cursor::new(&mut result), write_options::high()).unwrap();
+        image.write().to_buffered(Cursor::new(&mut result)).unwrap();
         bencher::black_box(result);
     })
 }
 
 benchmark_group!(write,
-    write_single_image_parallel,
-    write_single_image_uncompressed,
-    write_zip1,
-    write_zip16,
-    write_single_image
+    write_parallel_any_channels_to_buffered,
+    write_parallel_zip1_to_buffered,
+    write_parallel_zip16_to_buffered,
+    write_uncompressed_to_buffered
 );
 
 benchmark_main!(write);
