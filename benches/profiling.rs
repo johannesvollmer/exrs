00e614aa08abc5e50ec853f14d030427def8c19e
Sergey "Shnatsel" Davidoff <sdavydov@google.com>
2023-01-01 20:32:05 +0100

    More benchmarks

--- benches/profiling.rs
+++ benches/profiling.rs
@@ -25,9 +25,63 @@ fn read_single_image_all_channels(bench: &mut Bencher) {
     })
 }
 
+/// Read image from file
+fn read_single_image_from_buffer_all_channels(bench: &mut Bencher) {
+    let mut file = fs::read("tests/images/valid/custom/crowskull/crow_uncompressed.exr").unwrap();
+    bencher::black_box(&mut file);
+
+    bench.iter(||{
+        let image = exr::prelude::read()
+            .no_deep_data().largest_resolution_level()
+            .all_channels()
+            .all_layers().all_attributes()
+            .non_parallel()
+            .from_buffered(Cursor::new(file.as_slice())).unwrap();
+
+        bencher::black_box(image);
+    })
+}
+
+
 /// Read image from in-memory buffer
 fn read_single_image_from_buffer_rgba_channels(bench: &mut Bencher) {
-    let file = fs::read("tests/images/valid/custom/crowskull/crow_uncompressed.exr").unwrap();
+    let mut file = fs::read("tests/images/valid/custom/crowskull/crow_uncompressed.exr").unwrap();
+    bencher::black_box(&mut file);
+
+    bench.iter(||{
+        let image = exr::prelude::read()
+            .no_deep_data().largest_resolution_level()
+            .rgba_channels(PixelVec::<(f32,f32,f32,f32)>::constructor, PixelVec::set_pixel)
+            .all_layers().all_attributes()
+            .non_parallel()
+            .from_buffered(Cursor::new(file.as_slice())).unwrap();
+
+        bencher::black_box(image);
+    })
+}
+
+/// Read image from in-memory buffer and convert the samples to u32 (from native f32)
+fn read_single_image_from_buffer_rgba_channels_to_u32(bench: &mut Bencher) {
+    let mut file = fs::read("tests/images/valid/custom/crowskull/crow_uncompressed.exr").unwrap();
+    bencher::black_box(&mut file);
+
+    bench.iter(||{
+        let image = exr::prelude::read()
+            .no_deep_data().largest_resolution_level()
+            .rgba_channels(PixelVec::<(u32,u32,u32,u32)>::constructor, PixelVec::set_pixel)
+            .all_layers().all_attributes()
+            .non_parallel()
+            .from_buffered(Cursor::new(file.as_slice())).unwrap();
+
+        bencher::black_box(image);
+    })
+}
+
+/// Read image from in-memory buffer and convert the samples to u16 (from native f32)
+/// f16 is not natively supported by CPUs, which introduces unique performance pitfalls
+fn read_single_image_from_buffer_rgba_channels_to_f16(bench: &mut Bencher) {
+    let mut file = fs::read("tests/images/valid/custom/crowskull/crow_uncompressed.exr").unwrap();
+    bencher::black_box(&mut file);
 
     bench.iter(||{
         let image = exr::prelude::read()
@@ -41,10 +95,12 @@ fn read_single_image_from_buffer_rgba_channels(bench: &mut Bencher) {
     })
 }
 
-
 benchmark_group!(profiling,
-    read_single_image_from_buffer_rgba_channels,
     read_single_image_all_channels,
+    read_single_image_from_buffer_all_channels,
+    read_single_image_from_buffer_rgba_channels,
+    read_single_image_from_buffer_rgba_channels_to_u32,
+    read_single_image_from_buffer_rgba_channels_to_f16,
 );
 
 benchmark_main!(profiling);
